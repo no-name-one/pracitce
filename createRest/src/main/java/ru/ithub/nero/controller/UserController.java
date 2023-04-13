bc83335a562e499b2e2e28cb945e8bef15160f48
nero <nero@gmail.com>
2023-04-13 21:47:24 +0300

    added all http methods and request mapping

--- createRest/src/main/java/ru/ithub/nero/controller/UserController.java
+++ createRest/src/main/java/ru/ithub/nero/controller/UserController.java
@@ -1,23 +1,44 @@
 package ru.ithub.nero.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ru.ithub.nero.entity.User;
-import ru.ithub.nero.service.Impl.UserServiceImpl;
+import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.*;
+import ru.ithub.nero.model.UserDTO;
 import ru.ithub.nero.service.UserService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     private final UserService userService;
 
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/user")
-    public List<User> findAll() {return userService.findAll();}
+    @GetMapping
+    public List<UserDTO> findAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO getByID(@PathVariable UUID id) {
+        return userService.getByID(id);
+    }
+
+    @PostMapping
+    public UserDTO create(@RequestBody UserDTO userDTO) {
+        return userService.create(userDTO);
+    }
+
+    @PutMapping
+    public UserDTO update(@RequestBody UserDTO userDTO) {
+        return userService.update(userDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable UUID id) {
+        userService.deleteById(id);
+    }
 }
