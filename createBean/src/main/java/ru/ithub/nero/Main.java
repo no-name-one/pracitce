a107554569853ff7f401a172a92d5f02d182fec6
nero <nero@gmail.com>
2023-05-10 11:37:29 +0300

    update

--- createBean/src/main/java/ru/ithub/nero/Main.java
+++ createBean/src/main/java/ru/ithub/nero/Main.java
@@ -7,16 +7,14 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-//
-//        User user = context.getBean("myBean", User.class);
-//        System.out.println(user.getName());
-//
-//        User user2 = context.getBean(User.class);
-//        System.out.println(user2);
-//
-//        context.close();
-        LocalDate date = LocalDate.now();
-        System.out.println(date);
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        User user = context.getBean("myBean", User.class);
+        System.out.println(user.getName());
+
+        User user2 = context.getBean(User.class);
+        System.out.println(user2);
+
+        context.close();
     }
 }
