02b3b5c1a8e516e1ebaef4e0accd866ca5e398b7
nero <nero@gmail.com>
2023-05-12 19:18:50 +0300

    trying to use testcontainers - but it's failed

--- mockMVC/src/test/java/ru/ithub/nero/controller/UserControllerTest.java
+++ mockMVC/src/test/java/ru/ithub/nero/controller/UserControllerTest.java
@@ -1,6 +1,10 @@
 package ru.ithub.nero.controller;
 
+import io.restassured.RestAssured;
 import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,16 +14,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.testcontainers.containers.PostgreSQLContainer;
 import ru.ithub.nero.model.dto.UserDto;
 import ru.ithub.nero.repository.UserRepository;
 import org.hamcrest.CoreMatchers;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -31,6 +42,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(MockitoExtension.class)
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs(outputDir = "target/snippets")
+//@Testcontainers
 class UserControllerTest {
 
     @Autowired
@@ -42,14 +54,48 @@ class UserControllerTest {
     @InjectMocks
     private UserController userController;
 
+    @LocalServerPort
+    private Integer port;
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost:" + port;
+        userRepository.deleteAll();
+    }
+
     @Test
     public void getUsers_whenUsersExists_thenReturnUsers() throws Exception {
+        List<UserDto> users = List.of(
+                new UserDto(30L, "test30", 30, LocalDate.now()),
+                new UserDto(31L, "test31", 31, LocalDate.now()),
+                new UserDto(32L, "test32", 32, LocalDate.now())
+        );
+        userRepository.saveAll(users);
 
         mockMvc.perform(get("/user")
-                .contentType(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].name", CoreMatchers.is("Sun")))
+                .andExpect(jsonPath("$.size()", CoreMatchers.is(userRepository.getStorage().size())))
                 .andDo(MockMvcRestDocumentation.document("{class-name}/{method-name}"));
     }
 }
