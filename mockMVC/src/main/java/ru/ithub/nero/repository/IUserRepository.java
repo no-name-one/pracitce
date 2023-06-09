b34c3a0fb15cdb3ed1d79a8e60c0bc125c2e1988
nero <nero@gmail.com>
2023-06-09 17:47:38 +0300

    added sql query in repository

--- mockMVC/src/main/java/ru/ithub/nero/repository/IUserRepository.java
+++ mockMVC/src/main/java/ru/ithub/nero/repository/IUserRepository.java
@@ -1,10 +1,19 @@
 package ru.ithub.nero.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.ithub.nero.model.entity.User;
 
+import java.util.List;
+
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
+
+    @Query(value = "Select count(u) > 0 from User u where u.name like %:keyword")
+    boolean existsByName(@Param("keyword") String keyword);
 }
