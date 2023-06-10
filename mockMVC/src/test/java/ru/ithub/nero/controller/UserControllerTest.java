d8b3f8f5ce33d242023ed66086e05e8f41265972
nero <nero@gmail.com>
2023-06-10 13:10:15 +0300

    added rest docs and end2end tests

--- mockMVC/src/test/java/ru/ithub/nero/controller/UserControllerTest.java
+++ mockMVC/src/test/java/ru/ithub/nero/controller/UserControllerTest.java
@@ -1,6 +1,12 @@
 package ru.ithub.nero.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
+import jakarta.transaction.Transactional;
+import org.checkerframework.checker.units.qual.C;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,15 +21,29 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.shaded.org.hamcrest.CoreMatchers;
+import ru.ithub.nero.model.dto.CreateUserDto;
+import ru.ithub.nero.model.dto.UpdateUserDto;
+import ru.ithub.nero.model.dto.UserDto;
 import ru.ithub.nero.model.entity.User;
 import ru.ithub.nero.repository.IUserRepository;
 
@@ -31,6 +51,13 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -40,8 +67,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(MockitoExtension.class)
 @AutoConfigureMockMvc
-@AutoConfigureRestDocs(outputDir = "target/snippets")
+//@AutoConfigureRestDocs(outputDir = "target/snippets")
 @Testcontainers
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 class UserControllerTest {
 
     @Autowired
@@ -49,6 +77,12 @@ class UserControllerTest {
 
     @Mock
     private IUserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext context;
 
     @InjectMocks
     private UserController userController;
@@ -75,71 +109,180 @@ class UserControllerTest {
         registry.add("spring.datasource.password", postgres::getPassword);
     }
 
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/snippets");
+
     @BeforeEach
-    void setUp() {
+    void setUpDb() {
         RestAssured.baseURI = "http://localhost:" + port;
+    }
+
+    @BeforeEach
+    public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider provider) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(provider))
+                .build();
     }
 
     @Test
     public void getUsers_whenUsersExists_thenReturnUsers() throws Exception {
 
-        mockMvc.perform(get("/user")
+        MvcResult response = mockMvc.perform(RestDocumentationRequestBuilders.get("/user")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(print())
+                .andDo(document("getUsers"))
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(3));
+                .andReturn();
 
+        final List<UserDto> expectedUsersDto = new ArrayList<>();
+        expectedUsersDto.add(new UserDto(1L, "Tom", 11, LocalDate.now()));
+        expectedUsersDto.add(new UserDto(2L, "Jerry", 12, LocalDate.now()));
+        expectedUsersDto.add(new UserDto(3L, "Carry", 13, LocalDate.now()));
+
+
+        String entities = response.getResponse().getContentAsString();
+
+        List<UserDto> actualUsersDto = objectMapper.readValue(entities, new TypeReference<List<UserDto>>() {});
+
+        assertEquals(expectedUsersDto, actualUsersDto);
     }
 
     @Test
     public void getUser_whenUserExist_thenReturnUser() throws Exception {
-        mockMvc.perform(get("/user/{id}", 1)
+
+        MvcResult response = mockMvc.perform(RestDocumentationRequestBuilders.get("/user/{id}", 1)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(print())
+                .andDo(document("getUser",
+                        pathParameters(
+                                parameterWithName("id").description("The id of the input to get")
+                        )))
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Tom"));
+                .andReturn();
+
+        String entity = response.getResponse().getContentAsString();
+
+        UserDto expectedUserDto = UserDto
+                .builder()
+                .id(1L)
+                .name("Tom")
+                .age(11)
+                .date(LocalDate.now())
+                .build();
+
+        UserDto actualUserDto = objectMapper.readValue(entity, UserDto.class);
+
+        assertEquals(expectedUserDto, actualUserDto);
     }
 
-//    @Test
-//    public void createUser_whenUserDoesNotExist_thenReturnUser() throws Exception {
-//        String requestBody = "{\"name\":\"created User\",\"age\":20}";
-//
-//        mockMvc.perform(post("/user")
-//                        .content(requestBody)
-//                .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andDo(print())
-//                .andExpect(content()
-//                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("created User"));
-//    }
-
-//    @Test
-//    public void updateUser_whenUserExist_thenReturnUser() throws Exception {
-//        String requestBody2 = "{\"name\":\"updated name\",\"age\":20}";
-//
-//        mockMvc.perform(put("/user/{id}", 3)
-//                        .content(requestBody2)
-//                .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andDo(print())
-//                .andExpect(content()
-//                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("updated name"));
-//    }
-
     @Test
-    public void deleteUser_whenUserExist_thenReturnUser() throws Exception {
-        mockMvc.perform(delete("/user/{id}", 3)
+    @Transactional
+    @Rollback
+    public void createUser_whenUserDoesNotExist_thenReturnUser() throws Exception {
+        CreateUserDto createUserDto = CreateUserDto
+                .builder()
+                .name("created user")
+                .age(20)
+                .build();
+
+        String request = objectMapper.writeValueAsString(createUserDto);
+
+        MvcResult response = mockMvc.perform(RestDocumentationRequestBuilders.post("/user")
+                        .content(request)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(print())
+                .andDo(document("createUser"))
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Carry"));
+                .andReturn();
+
+        String entity = response.getResponse().getContentAsString();
+
+        UserDto expectedUserDto = UserDto
+                .builder()
+                .id(4L)
+                .name("created user")
+                .age(20)
+                .date(LocalDate.now())
+                .build();
+
+        UserDto actualUserDto = objectMapper.readValue(entity, UserDto.class);
+
+        assertEquals(expectedUserDto, actualUserDto);
+    }
+
+    @Test
+    public void updateUser_whenUserExist_thenReturnUser() throws Exception {
+        UpdateUserDto updateUserDto = UpdateUserDto
+                .builder()
+                .name("updated user")
+                .age(30)
+                .build();
+
+        String request = objectMapper.writeValueAsString(updateUserDto);
+
+        MvcResult response = mockMvc.perform(RestDocumentationRequestBuilders.put("/user/{id}", 3)
+                        .content(request)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("updateUser",
+                        pathParameters(
+                                parameterWithName("id").description("The id of the input to update")
+                        )))
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        String entity = response.getResponse().getContentAsString();
+
+        UserDto expectedUserDto = UserDto
+                .builder()
+                .id(3L)
+                .name("updated user")
+                .age(30)
+                .date(LocalDate.now())
+                .build();
+
+        UserDto actualUserDto = objectMapper.readValue(entity, UserDto.class);
+
+        assertEquals(expectedUserDto, actualUserDto);
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void deleteUser_whenUserExist_thenReturnUser() throws Exception {
+
+        MvcResult response = mockMvc.perform(RestDocumentationRequestBuilders.delete("/user/{id}", 3)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("deleteUser",
+                        pathParameters(
+                                parameterWithName("id").description("The id of the input to delete")
+                        )))
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        String entity = response.getResponse().getContentAsString();
+
+        UserDto expectedUserDto = UserDto
+                .builder()
+                .id(3L)
+                .name("Carry")
+                .age(13)
+                .date(LocalDate.now())
+                .build();
+
+        UserDto actualUserDto = objectMapper.readValue(entity, UserDto.class);
+
+        assertEquals(expectedUserDto, actualUserDto);
     }
 }
